40495a033cd88e7df22d3505bd3722142275bfa5
Rick Beerendonk <rick@beerendonk.com>
2019-11-26 22:51:41 +0100

    Improved naming.

--- operators/creation/create.js
+++ operators/creation/create.js
@@ -1,12 +1,12 @@
 const { Observable } = require('../../node_modules/rxjs');
 
-const hello = Observable.create(function(observer) {
+const obs = Observable.create(function(observer) {
   observer.next('One');
   observer.next('Two');
   observer.complete();
 });
 
-const subscribe = hello.subscribe(val => console.log(val));
+obs.subscribe(val => console.log(val));
 
 // One
 // Two
