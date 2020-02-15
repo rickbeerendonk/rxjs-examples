3dffa904059ec57a8c4c6c18bfb797bfb87945c6
Rick Beerendonk <rick@beerendonk.com>
2020-02-15 23:09:15 +0100

    Add "Operators - Mathematical and Aggregate - min - With comparer" example.

---
+++ 02 - Operators - TODO/10. Mathematical and Aggregate - TODO/min - ii. With comparer.js
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+const { from } = require('../../node_modules/rxjs');
+const { min } = require('../../node_modules/rxjs/operators');
+
+const obs1 = from([3, 7, 1, 5]);
+const obs2 = obs1.pipe(min((x, y) => (y < x ? -1 : 1))); // Reverse
+
+obs2.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// 1
+// Complete!
