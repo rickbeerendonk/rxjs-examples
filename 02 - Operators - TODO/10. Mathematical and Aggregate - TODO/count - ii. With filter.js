455fc0a474e82c3559e00b4aa2b46f0bec22645e
Rick Beerendonk <rick@beerendonk.com>
2020-02-15 12:36:07 +0100

    Add "Operators - Mathematical and Aggregate - count - With filter" example.

---
+++ 02 - Operators - TODO/10. Mathematical and Aggregate - TODO/count - ii. With filter.js
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+const { from } = require('../../node_modules/rxjs');
+const { count } = require('../../node_modules/rxjs/operators');
+
+const obs1 = from([1, 3, 5, 7]);
+const obs2 = obs1.pipe(count(x => x < 6));
+
+obs2.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// 3
+// Complete!
