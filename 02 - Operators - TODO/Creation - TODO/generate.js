1aae28adf4b859a56396fc2fb7244df2e339d930
Rick Beerendonk <rick@beerendonk.com>
2020-02-15 09:40:27 +0100

    Add "Operators - Creation - generate" example.

---
+++ 02 - Operators - TODO/Creation - TODO/generate.js
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+const { generate } = require('../../node_modules/rxjs');
+
+const obs = generate(
+  0, // initial
+  x => x < 10, // condition
+  x => x + 4, // iterate
+  x => x + 1 // result
+);
+
+obs.subscribe(val => console.log(val));
+
+// 1
+// 5
+// 9
