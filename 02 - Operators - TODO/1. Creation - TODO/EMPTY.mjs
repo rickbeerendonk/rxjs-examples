73573abdb264b2494017f2ef0363aa9a19317a6c
Rick Beerendonk <rick@beerendonk.com>
2022-11-14 02:02:57 +0100

    Add "EMPTY" example.

---
+++ 02 - Operators - TODO/1. Creation - TODO/EMPTY.mjs
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { EMPTY } from 'rxjs';
+
+const obs = EMPTY;
+
+obs.subscribe({
+  next: () => console.log('Next'),
+  complete: () => console.log('Complete!')
+});
