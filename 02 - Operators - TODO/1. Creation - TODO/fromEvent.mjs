8de297410d78586d766d5202eb2881412821c98e
Rick Beerendonk <rick@beerendonk.com>
2022-12-17 18:14:20 +0100

    Add "Operators - Creation - fromEvent" example.

---
+++ 02 - Operators - TODO/1. Creation - TODO/fromEvent.mjs
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import { EventEmitter } from 'events';
+import { fromEvent } from 'rxjs';
+
+const myEventEmitter = new EventEmitter();
+
+// myEventEmitter.on('dummyEvent', data => {
+//   console.log('--remove--', data);
+// });
+
+const obs$ = fromEvent(myEventEmitter, 'dummyEvent');
+
+obs$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// Trigger event
+myEventEmitter.emit('dummyEvent', 'Dummy Data');
+
+// Dummy Data
