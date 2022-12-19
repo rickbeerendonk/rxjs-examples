10c20ee4ae75e9b048196f9a1fa7d0d35f258303
Rick Beerendonk <rick@beerendonk.com>
2022-12-19 18:34:12 +0100

    Update versions.

--- 02 - Operators - TODO/1. Creation - TODO/ajax [WIP].mjs
+++ 02 - Operators - TODO/1. Creation - TODO/ajax [WIP].mjs
@@ -1,19 +1,17 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import { ajax } from 'rxjs/ajax/index.js';
+import { ajax } from 'rxjs/ajax';
 import { XMLHttpRequest } from 'xmlhttprequest';
-
-function createXHR() {
-  return new XMLHttpRequest();
-}
 
 // Short:
 // const obs$ = ajax('https://jsonplaceholder.typicode.com/posts');
 
 // Long:
 const obs$ = ajax({
-  createXHR,
+  createXHR: () => new XMLHttpRequest(),
+  method: 'GET',
+  responseType: 'json',
   url: 'https://jsonplaceholder.typicode.com/posts',
   crossDomain: true
 });
--- 02 - Operators - TODO/1. Creation - TODO/throwError.mjs
+++ 02 - Operators - TODO/1. Creation - TODO/throwError.mjs
@@ -3,7 +3,7 @@
 
 import { throwError } from 'rxjs';
 
-const obs$ = throwError(new Error('Something went wrong!'));
+const obs$ = throwError(() => new Error('Something went wrong!'));
 
 obs$.subscribe({
   error: err => console.error('Error message received: ' + err)
