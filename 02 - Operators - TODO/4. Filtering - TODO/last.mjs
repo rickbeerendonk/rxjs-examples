cf94a218330a5afe2e9323bd8ddaa11c86beaf2c
Rick Beerendonk <rick@beerendonk.com>
2023-01-05 00:15:41 +0100

    Add "Operators - Filtering - last" example.

---
+++ 02 - Operators - TODO/4. Filtering - TODO/last.mjs
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { of, last } from 'rxjs';
+
+const obs$ = of('a', 'b', 'c').pipe(last());
+
+obs$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// a
+// Complete!
