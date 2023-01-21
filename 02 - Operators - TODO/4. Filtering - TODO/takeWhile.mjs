9b74e0ed19a3b78925ec174aa6c594e69dad8fb8
Rick Beerendonk <rick@beerendonk.com>
2023-01-22 00:29:17 +0100

    Add "Operators - Filtering - takeWhile" example.

---
+++ 02 - Operators - TODO/4. Filtering - TODO/takeWhile.mjs
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { of, takeWhile } from 'rxjs';
+
+const obs$ = of(1, 2, 3, 4, 5).pipe(takeWhile(val => val < 4));
+
+obs$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// 1
+// 2
+// 3
+// Complete!
