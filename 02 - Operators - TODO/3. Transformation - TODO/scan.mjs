c5037ae430fc7eb2bca02d912b88a1001e8ef0ee
Rick Beerendonk <rick@beerendonk.com>
2023-01-24 01:50:03 +0100

    Add "Operators - Transformation - scan" example.

---
+++ 02 - Operators - TODO/3. Transformation - TODO/scan.mjs
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { from } from 'rxjs';
+import { scan } from 'rxjs/operators';
+
+const obs1$ = from([1, 2, 3, 4]);
+const obs2$ = obs1$.pipe(scan((acc, value, index) => acc + value));
+
+obs2$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// 1
+// 3
+// 6
+// 10
+// Complete!
