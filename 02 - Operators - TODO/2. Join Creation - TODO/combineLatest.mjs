a3488ffad672205500a3159cbd3b5d2d27241025
Rick Beerendonk <rick@beerendonk.com>
2023-01-24 21:52:56 +0100

    Add "Operators - Join Creation - combineLatest" example.

---
+++ 02 - Operators - TODO/2. Join Creation - TODO/combineLatest.mjs
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { from, combineLatest } from 'rxjs';
+
+const obs1$ = from([1, 3, 5]);
+const obs2$ = from([2, 4, 6]);
+
+const obs$ = combineLatest([obs1$, obs2$]);
+
+obs$.subscribe({
+  next: val => console.log(val),
+  complete: () => console.log('Complete!')
+});
+
+// [ 5, 2 ]
+// [ 5, 4 ]
+// [ 5, 6 ]
+// Complete!
