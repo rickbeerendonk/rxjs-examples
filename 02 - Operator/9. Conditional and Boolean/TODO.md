731b31aeab034c1555e69917100d11b3adc719a6
Rick Beerendonk <rick@beerendonk.com>
2024-09-15 15:32:00 +0200

    Add "Operator - Conditional and Boolean - defaultIfEmpty" example.

--- 02 - Operator/9. Conditional and Boolean/TODO.md
+++
@@ -1,5 +0,0 @@
-defaultIfEmpty
-every
-find
-findIndex
-isEmpty
--- 02 - Operator/9. Conditional and Boolean/defaultIfEmpty.mjs
+++ 02 - Operator/9. Conditional and Boolean/defaultIfEmpty.mjs
@@ -1,24 +1,25 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
-import { EMPTY, isEmpty, of } from 'rxjs';
+import { defaultIfEmpty, EMPTY, of } from 'rxjs';
 
-const obs1$ = of(1).pipe(isEmpty());
+const obs1$ = of(1, 2).pipe(defaultIfEmpty(99));
 
 obs1$.subscribe({
   next: val => console.log(val),
   complete: () => console.log('Complete 1')
 });
 
-// false
+// 1
+// 2
 // Complete 1
 
-const obs2$ = EMPTY.pipe(isEmpty());
+const obs2$ = EMPTY.pipe(defaultIfEmpty(99));
 
 obs2$.subscribe({
   next: val => console.log(val),
   complete: () => console.log('Complete 2')
 });
 
-// true
+// 99
 // Complete 2
