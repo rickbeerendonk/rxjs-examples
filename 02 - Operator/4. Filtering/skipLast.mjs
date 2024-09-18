3081c7f78fc4b0685bba642e61455e6c5c676c48
Rick Beerendonk <rick@beerendonk.com>
2024-09-18 04:16:54 +0200

    Make async.

--- 02 - Operator/4. Filtering/skipLast.mjs
+++ 02 - Operator/4. Filtering/skipLast.mjs
@@ -1,9 +1,10 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2023 Rick Beerendonk          !*/
 
-import { of, skipLast } from 'rxjs';
+import { of, skipLast, interval } from 'rxjs';
 
-const obs$ = of(1, 2, 3, 4, 5).pipe(skipLast(2));
+const obs$ = interval(1000).pipe(skipLast(2));
+//const obs$ = of(1, 2, 3, 4, 5).pipe(skipLast(2));
 
 obs$.subscribe({
   next: val => console.log(val),
